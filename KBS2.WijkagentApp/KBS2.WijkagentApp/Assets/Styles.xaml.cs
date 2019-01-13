d34cd6923774f72663fa7320a80b2435f54282ad
Karen Bosch - Brakband <info@karenbosch.net>
2019-01-13 21:37:37 +0100

    Added a separate file for the ResourceDictionary and styled the NewNoticePage

---
+++ KBS2.WijkagentApp/KBS2.WijkagentApp/Assets/Styles.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KBS2.WijkagentApp.Assets
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Styles : ResourceDictionary
+	{
+		public Styles ()
+		{
+			
+		}
+	}
+}
