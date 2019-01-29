26b5b40ff8c1383fe79cffd0c82b78561d8e451c
DESKTOP-P03HJ5A\rvanb <rvanbostelen@outlook.com>
2019-01-29 15:08:05 +0100

    Gezeik van Ronald :-)

---
+++ KBS2.WijkagentApp/KBS2.WijkagentApp/ViewModels/AntecedentsInvestigationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using KBS2.WijkagentApp.Assets;
+using KBS2.WijkagentApp.DataModels.old;
+using KBS2.WijkagentApp.ViewModels.Commands;
+using Xamarin.Forms;
+
+namespace KBS2.WijkagentApp.ViewModels
+{
+    class AntecedentsInvestigationViewModel : BaseViewModel
+    {
+        public AntecedentsInvestigationViewModel()
+        {
+
+        }
+    }
+}
